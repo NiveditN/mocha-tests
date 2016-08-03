64d1bba7e9f71e259a60cb7c0aa8ff14b49dc603
NiveditN <niveditn@gmail.com>
2016-08-03 12:10:59 +0530

    Step 2: should return the same array

--- test.js
+++ test.js
@@ -7,10 +7,10 @@ describe('String#split', function() {
 
 	it('should return the same array', function() {
 		assert.equal(['a', 'b', 'c'].length, 
-			['a,b,c'].split(','.length, 'arrays have equal length'));
+			'a,b,c'.split(',').length, 'arrays have equal length');
 		for(var i=0; i<['a','b','c'].length; i++) {
 			assert.equal(['a','b','c'][i],
-				['a,b,c'].split(',')[i], i + 'element is equal');
+				'a,b,c'.split(',')[i], i + 'element is equal');
 		};
 	});
 
